7dbbee239e234c0f17ba1191782fc07476f41583
Emma <emma.grace.burgess@gmail.com>
2026-01-30 15:16:47 +0000

    final changes

--- config/model_config.py
+++ config/model_config.py
@@ -1,20 +1,20 @@
 """NLLB-200 language code mappings."""
 
 LANGUAGE_CODES = {
-  'Chinese': 'zho_Hans',
-  'English': 'eng_Latn',
-  'Japanese': 'jpn_Jpan',
-  'Korean': 'kor_Hang',
-  'German': 'deu_Latn',
-  'French': 'fra_Latn',
-  'Russian': 'rus_Cyrl',
-  'Portuguese': 'por_Latn',
-  'Spanish': 'spa_Latn',
-  'Italian': 'ita_Latn'
+    'Chinese':      'zho_Hans',
+    'English':      'eng_Latn',
+    'Japanese':     'jpn_Jpan',
+    'Korean':       'kor_Hang',
+    'German':       'deu_Latn',
+    'French':       'fra_Latn',
+    'Russian':      'rus_Cyrl',
+    'Portuguese':   'por_Latn',
+    'Spanish':      'spa_Latn',
+    'Italian':      'ita_Latn'
 }
 
-MODEL_CONFIGS = {
+TRANSLATION_MODELS = {
     'original': 'facebook/nllb-200-3.3B',
-    'green': 'facebook/nllb-200-distilled-1.3B',
-    'tiny': 'facebook/nllb-200-distilled-600M',
+    'green':    'facebook/nllb-200-distilled-1.3B',
+    'tiny':     'facebook/nllb-200-distilled-600M',
 }
