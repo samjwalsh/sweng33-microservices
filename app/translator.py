1ab723059b8a0321f9b522f6d833badabecaf6e3
Emma <emma.grace.burgess@gmail.com>
2026-01-30 15:16:47 +0000

    final changes

--- app/translator.py
+++ app/translator.py
@@ -1,10 +1,7 @@
 import os
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import torch
-
-MODEL_ORIGINAL = "facebook/nllb-200-3.3B"
-MODEL_GREEN = "facebook/nllb-200-distilled-1.3B" # distilled means compressed - less processing power = green computing
-MODEL_TINY = "facebook/nllb-200-distilled-600M" # using this smaller model for testing to save compute
+from config.model_config import TRANSLATION_MODELS, LANGUAGE_CODES
 
 def load_translator(model_name):
     hf_token = os.getenv("HF_TOKEN")
@@ -12,19 +9,19 @@ def load_translator(model_name):
     if not hf_token:
         raise ValueError("Hugging Face token not found! Set HF_TOKEN as an environment variable.")
     
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(TRANSLATION_MODELS[model_name], torch_dtype="auto", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(TRANSLATION_MODELS[model_name])
     return model, tokenizer
 
 def translate_text(model, tokenizer, text, language_from, language_to):
-    tokenizer.src_lang = language_from  
+    tokenizer.src_lang = LANGUAGE_CODES[language_from]
     inputs = tokenizer(text, return_tensors="pt").to(model.device)
-    forced_bos_token_id = tokenizer.convert_tokens_to_ids(language_to)
+    forced_bos_token_id = tokenizer.convert_tokens_to_ids(LANGUAGE_CODES[language_to])
 
     with torch.no_grad():
         gen_tokens = model.generate(**inputs, forced_bos_token_id=forced_bos_token_id)
 
     return tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
 
-# model, tokenizer = load_translator(MODEL_TINY)
+# model, tokenizer = load_translator(tiny)
 # print(translate_text(model, tokenizer, "good morning", "eng_Latn", "fra_Latn"))
