c4d5b4f95c494c19b17007da275fea23b00a238b
Emma <emma.grace.burgess@gmail.com>
2026-01-30 14:57:56 +0000

    changing configs

---
+++ config/model_config.py
@@ -0,0 +1,20 @@
+"""NLLB-200 language code mappings."""
+
+LANGUAGE_CODES = {
+  'Chinese': 'zho_Hans',
+  'English': 'eng_Latn',
+  'Japanese': 'jpn_Jpan',
+  'Korean': 'kor_Hang',
+  'German': 'deu_Latn',
+  'French': 'fra_Latn',
+  'Russian': 'rus_Cyrl',
+  'Portuguese': 'por_Latn',
+  'Spanish': 'spa_Latn',
+  'Italian': 'ita_Latn'
+}
+
+MODEL_CONFIGS = {
+    'original': 'facebook/nllb-200-3.3B',
+    'green': 'facebook/nllb-200-distilled-1.3B',
+    'tiny': 'facebook/nllb-200-distilled-600M',
+}
