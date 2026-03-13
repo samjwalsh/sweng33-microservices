0a1256b2d97f44f57d30f35fa2ccf3463e3d7466
Emma <emma.grace.burgess@gmail.com>
2026-03-13 17:35:28 +0000

    removing test

--- tests/storage/test_blob_health.py
+++ tests/storage/test_blob_health.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-
+"""
 @pytest.mark.skipif(
     not connection_string,
     reason="Requires Azure Blob Storage access"
 )
 def test_blob_health():
     assert BlobHealthCheck.run() is True
+"""
